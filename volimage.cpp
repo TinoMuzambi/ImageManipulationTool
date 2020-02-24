db5d913a10541df15a9fc51777b748866c1b07b5
Tino <tinomuzambi@gmail.com>
2020-02-24 10:33:48 +0200

    Added method stubs.

--- volimage.cpp
+++ volimage.cpp
@@ -12,10 +12,48 @@ int main(int argc, char* argv[]) {
         line2 = argv[3];
     }
     catch (int e) {}
-    prefix += ".data";
-    ifstream in(prefix);
+
+    ifstream in(prefix + ".data");
     string header;
     getline(in, header);
     cout << header << endl;
+    stringstream ss(header);
+    string token;
+    vector<int> dims; // Vector that stores the ints in the data file.
+    while (ss >> token) {
+        stringstream ss2(token);
+        int curr;
+        ss2 >> curr;
+        dims.push_back(curr);
+    }
+    for (int i = 0; i < dims[0]; i++) {
+        // do valuable stuff.
+    }
+    return 0;
+}
+
+VolImage::VolImage() {
+    VolImage::width = 0;
+    VolImage::height = 0;
+    VolImage::slices.clear();
+}
+
+VolImage::~VolImage() {
+    // delete stuff
+}
+
+bool VolImage::readImages(string baseName) {
+    return false;
+}
+
+void VolImage::diffmap(int sliceI, int sliceJ, string output_prefix) {
+
+}
+
+void VolImage::extract(int sliceId, string output_prefix) {
+
+}
+
+int VolImage::volImageSize(void) {
     return 0;
 }
